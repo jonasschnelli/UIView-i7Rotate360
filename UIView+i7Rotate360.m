fc8baceffb55ee9695b1b1e7d319585f3ce3cd22
terryso <suchuanyi@126.com>
2014-02-08 12:40:32 +0800

    add rotate direction

--- UIView+i7Rotate360.m
+++ UIView+i7Rotate360.m
@@ -11,13 +11,22 @@
 
 @implementation UIView (i7Rotate360)
 
-- (void)rotate360WithDuration:(CGFloat)aDuration repeatCount:(CGFloat)aRepeatCount timingMode:(enum i7Rotate360TimingMode)aMode {
+- (void)rotate360WithDuration:(CGFloat)aDuration repeatCount:(CGFloat)aRepeatCount timingMode:(enum i7Rotate360TimingMode)aMode rotateDirection:(enum i7Rotate360RotateDirection)aDirection {
 	CAKeyframeAnimation *theAnimation = [CAKeyframeAnimation animation];
-	theAnimation.values = [NSArray arrayWithObjects:
-						   [NSValue valueWithCATransform3D:CATransform3DMakeRotation(0, 0,0,1)],
-						   [NSValue valueWithCATransform3D:CATransform3DMakeRotation(3.13, 0,0,1)],
-						   [NSValue valueWithCATransform3D:CATransform3DMakeRotation(6.26, 0,0,1)],
-						   nil];
+    if (aDirection == i7Rotate360RotateDirectionClockwise) {
+        theAnimation.values = [NSArray arrayWithObjects:
+                                               [NSValue valueWithCATransform3D:CATransform3DMakeRotation(0, 0, 0, 1)],
+                                               [NSValue valueWithCATransform3D:CATransform3DMakeRotation(3.13, 0, 0, 1)],
+                                               [NSValue valueWithCATransform3D:CATransform3DMakeRotation(6.26, 0, 0, 1)],
+                                               nil];
+    } else {
+        theAnimation.values = [NSArray arrayWithObjects:
+                                               [NSValue valueWithCATransform3D:CATransform3DMakeRotation(0, 0, 0, 1)],
+                                               [NSValue valueWithCATransform3D:CATransform3DMakeRotation(-3.13, 0, 0, 1)],
+                                               [NSValue valueWithCATransform3D:CATransform3DMakeRotation(-6.26, 0, 0, 1)],
+                                               nil];
+    }
+
 	theAnimation.cumulative = YES;
 	theAnimation.duration = aDuration;
 	theAnimation.repeatCount = aRepeatCount;
@@ -33,11 +42,11 @@
 }
 
 - (void)rotate360WithDuration:(CGFloat)aDuration timingMode:(enum i7Rotate360TimingMode)aMode {
-	[self rotate360WithDuration:aDuration repeatCount:1 timingMode:aMode];
+	[self rotate360WithDuration:aDuration repeatCount:1 timingMode:aMode rotateDirection:i7Rotate360RotateDirectionClockwise];
 }
 
 - (void)rotate360WithDuration:(CGFloat)aDuration {
-	[self rotate360WithDuration:aDuration repeatCount:1 timingMode:i7Rotate360TimingModeEaseInEaseOut];
+	[self rotate360WithDuration:aDuration repeatCount:1 timingMode:i7Rotate360TimingModeEaseInEaseOut rotateDirection:i7Rotate360RotateDirectionClockwise];
 }
 
 @end
